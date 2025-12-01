3ce62055b9c63e9a986dd8c110da5683b82d805e
karasikuk <26507468+basenc@users.noreply.github.com>
2025-12-01 18:46:40 +0400

    Update Next.js configuration and adjust manifest paths

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: isProd ? "/chatbot-ui" : "",
+  assetPrefix: isProd ? "/chatbot-ui/" : "",
   reactCompiler: true,
   images: {
     unoptimized: true,
